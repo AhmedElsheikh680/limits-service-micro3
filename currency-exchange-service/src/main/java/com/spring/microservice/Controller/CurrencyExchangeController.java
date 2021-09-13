cad812a27a9bd9dbc7b18e490065c5ab4e67fc99
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-09-13 11:53:02 +0200

    load balancing(adding one than one port) and using Environment

--- currency-exchange-service/src/main/java/com/spring/microservice/Controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/spring/microservice/Controller/CurrencyExchangeController.java
@@ -1,17 +1,25 @@
 package com.spring.microservice.Controller;
 
 import com.spring.microservice.model.CurrencyExchange;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 
+
 @RestController
 public class CurrencyExchangeController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to){
-        return  new CurrencyExchange(1001L, "USD", "INR", BigDecimal.valueOf(50));
+          CurrencyExchange currencyExchange = new CurrencyExchange(1001L, "USD", "INR", BigDecimal.valueOf(50));
+          currencyExchange.setEnvironment(environment.getProperty("server.port"));
+          return currencyExchange;
     }
 }
