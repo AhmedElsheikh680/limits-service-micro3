0b4968f84928b8b2532333336436ce6e41ca3079
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-09-19 10:40:08 +0200

    circuit breaker #2

--- currency-exchange-service/src/main/java/com/spring/microservice/Controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/spring/microservice/Controller/CircuitBreakerController.java
@@ -12,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class CircuitBreakerController {
 
+
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
     @GetMapping("/sample-api")
